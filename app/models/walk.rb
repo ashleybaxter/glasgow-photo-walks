64b24de16ee9c60038d83fbf65f73fe826b46380
ashleybaxter <ashleybaxter@me.com>
2013-12-21 10:31:36 +0000

    Start time / End time calculation

--- app/models/walk.rb
+++ app/models/walk.rb
@@ -2,4 +2,10 @@ class Walk < ActiveRecord::Base
 	geocoded_by :address
 
 	after_validation :geocode
+	
+	def duration
+	  e = end_time.strftime("%k:%M")
+	  s = start_time.strftime("%k:%M")
+	  (e.to_time - s.to_time) / 3600
+  end
 end
