5b14134c3d302186b8c71725fb679c35761f9c23
Ashley Baxter <ashleybaxter@me.com>
2013-12-23 14:59:42 +0000

    Removed secret token from .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GlasgowPhotoWalks::Application.config.secret_key_base = 'c869f3b5c36dbf95b4fb83a34ce357a795b0ee2664dcde721223bd1c00272a681dfd9ae3fdbcfa0ed500de90dc3d8c55a472f1bb793fd7391d833dd284e279ac'
