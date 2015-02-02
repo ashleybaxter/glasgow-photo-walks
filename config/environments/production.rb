1a7ab82899ebcdf8ba0b2734d27798c75b3c47ea
ashleybaxter <ashleybaxter@me.com>
2015-02-02 16:57:02 +0000

    Host set for Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 GlasgowPhotoWalks::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.default_url_options = { :host => 'glasgowphotowalk.co.uk' }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
